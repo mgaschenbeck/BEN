059d00c7609d79392efe7a7ff358a83e87801b88
Michael Aschenbeck <maschenb@r02n05.ux.dg.local>
2020-02-13 16:23:00 +0000

    Fixed something

--- Source/Losses.py
+++ Source/Losses.py
@@ -1,6 +1,14 @@
 import tensorflow.keras.backend as K
 import tensorflow as tf
-def get_weighted_loss(weights):
+
+
+#This weights false positives and false negatives differently.
+#Example:
+#        model.compile(optimizer=opt,loss=mga_get_weighted_loss(np.array([[1.,20.]])),metrics=[f1,precision,recall,'binary_accuracy'])
+
+
+def mga_get_weighted_loss(weights):
     def weighted_loss(y_true, y_pred):
-        return K.mean((weights[:,0]**(1-y_true))*(weights[:,1]**(y_true))*K.binary_crossentropy(K.round(y_true), K.round(y_pred)), axis=-1)
+        return K.mean(K.cast((weights[:,0]**(1-y_true))*(weights[:,1]**(y_true)),dtype='float32')*K.cast(K.binary_crossentropy(K.cast(y_true,dtype='float32'), K.cast(y_pred,dtype='float32')),dtype='float32'), axis=-1)
     return weighted_loss
+
